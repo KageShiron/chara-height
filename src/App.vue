ac5101af092485cdc06af224a1f0d21f27d92cf0
KageShiron <kageshiron@gmail.com>
2017-02-18 14:17:27 +0900

    Implement sex-age

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <sex-age />
+    <sex-age @apply="apply" />
     <chart-data :average="150" :stddev="8.0" />
     <chart :average="150" :stddev="8.0" />
   </div>
 </template>
 
 <script>
-import SexAge from "./SexAge.vue";
-import Chart from "./Chart.vue";
-import ChartData from "./ChartData.vue";
+  import SexAge from "./SexAge.vue";
+  import Chart from "./Chart.vue";
+  import ChartData from "./ChartData.vue";
 
-export default {
-  name: 'app',
-  data () {
-    return {
-      sex:0,
-      age:0,
-      average:0,
-      stddev:0
-    }
-  },
-  components : { "sex-age" : SexAge , "chart" : Chart , "chart-data" : ChartData }
-}
+  export default {
+    name: 'app',
+    data() {
+      return {
+        sex: 0,
+        age: 0,
+        average: 0,
+        stddev: 0
+      }
+    },
+    methods: {
+      apply: function (args) {
+        this.average = args.average;
+        this.stddev = args.stddev;
+      }
+    },
+    components: { "sex-age": SexAge, "chart": Chart, "chart-data": ChartData }
+  }
+
+
 </script>
 
 <style lang="scss">
--- src/SexAge.vue
+++ src/SexAge.vue
@@ -2,14 +2,15 @@
     <div id="sex-age">
         <h2>生物学的性別</h2>
         <form>
-            <input id="male" type="radio" />
+            <input id="male" type="radio" name="sex" value="1" v-model="sex" />
             <label for="male">男</label>
-            <input id="female" type="radio" />
+            <input id="female" type="radio" name="sex" value="2" v-model="sex" />
             <label for="female">女</label>
         </form>
         <h2>年齢(学年)</h2><br> 4/1地点の年齢
         <br>
-        <select>
+        <select v-model="age">
+            <option value="0">--選択--</option>
             <option value="5">5歳 (幼稚園)</option>
             <option value="6">6歳 (小1)</option>
             <option value="7">7歳 (小2)</option>
@@ -24,15 +25,51 @@
             <option value="16">16歳(高2)</option>
             <option value="17">17歳(高3)</option>
         </select>
-        <input type="button" value="反映" />
+        <input type="button" value="反映" @click="apply" />
     </div>
 </template>
 
 <script>
+
+    const toukei = [{}, {
+        5: { "average": 110.4, "stddev": 4.71 },
+        6: { "average": 116.5, "stddev": 4.87 },
+        7: { "average": 122.5, "stddev": 5.19 },
+        8: { "average": 128.1, "stddev": 5.41 },
+        9: { "average": 133.5, "stddev": 5.63 },
+        10: { "average": 138.9, "stddev": 6.16 },
+        11: { "average": 145.2, "stddev": 7.08 },
+        12: { "average": 152.6, "stddev": 8.04 },
+        13: { "average": 159.8, "stddev": 7.68 },
+        14: { "average": 165.1, "stddev": 6.69 },
+        15: { "average": 168.3, "stddev": 5.96 },
+        16: { "average": 169.8, "stddev": 5.82 },
+        17: { "average": 170.7, "stddev": 5.78 },
+    }
+        , {
+        5: { "average": 109.4, "stddev": 4.66 },
+        6: { "average": 115.5, "stddev": 4.83 },
+        7: { "average": 121.5, "stddev": 5.13 },
+        8: { "average": 127.3, "stddev": 5.50 },
+        9: { "average": 133.4, "stddev": 6.14 },
+        10: { "average": 140.1, "stddev": 6.77 },
+        11: { "average": 146.7, "stddev": 6.63 },
+        12: { "average": 151.8, "stddev": 5.90 },
+        13: { "average": 154.9, "stddev": 5.44 },
+        14: { "average": 156.5, "stddev": 5.30 },
+        15: { "average": 157.1, "stddev": 5.29 },
+        16: { "average": 157.6, "stddev": 5.32 },
+        17: { "average": 157.9, "stddev": 5.38 },
+    }
+
+    ]
     export default {
         name: 'sex-age',
-        data() {
-            return {
+        data() { return { sex: 1, age: 0 } },
+        methods: {
+            apply: function () {
+                const v = toukei[this.sex][this.age];
+                this.$emit("apply", { average: v.average, stddev: v.stddev });
             }
         }
     }
